6507b78ff43f0f48288137c10fa6af269a519e6a
theguigs <48953737+theguigs@users.noreply.github.com>
2021-01-25 14:10:35 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/mapbox/mapbox-events-ios", from: "0.10.70"),
+        .package(url: "https://github.com/mapbox/mapbox-events-ios", from: "0.10.7"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Mapbox-iOS-SDK",
-            dependencies: [])
+            path: "platform/ios")
     ]
 )
